5d8b98b5f6063ff9f4ce84d5fcd4f5d5b16dad02
enj0yer <90058431+enj0yer@users.noreply.github.com>
2022-09-21 14:54:17 +0400

    refactor: Renamed variables

--- classes/Ban.py
+++ classes/Ban.py
@@ -2,12 +2,12 @@ from datetime import datetime, timedelta
 
 
 class Ban:
-    def __init__(self, ban_id: int = None, user_id: int = None, cause: str = None, start_ban_time: datetime = None, ban_duration: int = None):
+    def __init__(self, ban_id: int = None, user_id: int = None, cause: str = None, ban_starts: datetime = None, ban_duration: int = None):
         self.__id = ban_id
         self.__user_id = user_id
         self.__cause = cause
-        self.__start_ban_time = start_ban_time
-        self.__end_ban_time = self.__calculate_unban_time(start_ban_time, ban_duration)
+        self.__ban_starts = ban_starts
+        self.__ban_ends = self.__calculate_unban_time(ban_starts, ban_duration)
 
     @staticmethod
     def __calculate_unban_time(start_ban_time, ban_duration: int) -> datetime:
@@ -23,20 +23,20 @@ class Ban:
 
     @property
     def start_ban_time(self):
-        return self.__start_ban_time
+        return self.__ban_starts
 
     @property
     def end_ban_time(self) -> datetime:
-        return self.__end_ban_time
+        return self.__ban_ends
 
     @property
     def ban_duration(self) -> timedelta.seconds:
-        return self.__end_ban_time - self.__start_ban_time
+        return self.__ban_ends - self.__ban_starts
 
     @ban_duration.setter
     def ban_duration(self, new_ban_duration) -> None:
-        self.__end_ban_time = self.__calculate_unban_time(self.__start_ban_time, new_ban_duration)
+        self.__ban_ends = self.__calculate_unban_time(self.__ban_starts, new_ban_duration)
 
     def check_ban_state(self):
-        return datetime.now() < self.__end_ban_time
+        return datetime.now() < self.__ban_ends
 
