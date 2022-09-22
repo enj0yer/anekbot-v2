e580d637b94cfe370368c679687dd06e6083128a
enj0yer <90058431+enj0yer@users.noreply.github.com>
2022-09-22 20:09:13 +0400

    fix: fixed query feat: added method

--- controllers/BansController.py
+++ controllers/BansController.py
@@ -11,4 +11,8 @@ class BansController(Connection):
         super().__del__()
 
     def add_new_ban(self, user: User, ban: Ban):
-        self._cursor.execute("insert into bans (user_id, cause, unban_time) values (?, ?, ?)", (user.user_id, ban.))
+        self._cursor.execute("insert into bans (user_id, cause, ban_starts, ban_ends) values (?, ?, ?, ?)", (user.user_id, ban.cause, ban.ban_starts, ban.ban_ends,))
+
+    def remove_ban(self, ban: Ban):
+        self._cursor.execute("delete from bans where id = (?)", (ban.id,))
+
