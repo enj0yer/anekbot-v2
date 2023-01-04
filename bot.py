b5233d2efdb07b2b2fc650af4fccb74696eeeccd
Enj0yer <kulagin2002ilya@gmail.com>
2023-01-04 22:44:01 +0400

    fix welcome script

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from classes.base import User
 from classes.controllers import BansController, UsersController, JokesController, LikesController
 from config.settings import TOKEN
+from classes.enums import Role
 
 logging.basicConfig(level=logging.INFO)
 
@@ -13,19 +14,24 @@ db = Dispatcher(bot)
 @db.message_handler(commands=['start'])
 async def welcome(message: types.Message):
     controller = UsersController()
-    controller.save(User(tg_id=message.from_user.id,
-                         username=message.from_user.username,
-                         first_name=message.from_user.first_name,
-                         last_name=message.from_user.last_name))
+    user = controller.get_by_tg_id(message.from_user.id)
+    if not user:
+        controller.save(User(tg_id=message.from_user.id,
+                             username=message.from_user.username,
+                             first_name=message.from_user.first_name,
+                             last_name=message.from_user.last_name,
+                             role=Role.USER))
+    else:
+        controller.save(User(user_id=user.id, username=message.from_user.username, first_name=message.from_user.first_name, last_name=message.from_user.last_name, role=user.role))
 
     await message.answer(f"Приветствую, {message.from_user.username}.\n/joke - очередной анекдот")
 
 
 @db.message_handler(commands=['joke'])
 async def get(message: types.Message):
-    ban = UsersController.is_banned(tg_id=message.from_user.id)
+    ban = UsersController.is_banned(User(tg_id=message.from_user.id))
     if ban:
-        await message.answer(f'Вы были забанены {("модератором " + ban.from_user) if ban.from_user else ""} {("по причине " + "`" + ban.cause + "`") if ban.cause else ""}')
+        await message.answer(f'Вы были забанены {("модератором " + str(UsersController().get_by_id(ban.from_user).username)) if ban.from_user else ""} {("по причине " + "`" + ban.cause + "`") if ban.cause else ""}')
         return
     controller = JokesController()
     random_joke = controller.get_random_joke()
