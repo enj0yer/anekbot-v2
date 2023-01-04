36921b3b238fa8ceb52b6d7ebaf3baa5a82497fe
Enj0yer <kulagin2002ilya@gmail.com>
2023-01-04 22:51:02 +0400

    add docs add BanType enum

--- classes/enums.py
+++ classes/enums.py
@@ -2,17 +2,20 @@ from enum import Enum
 
 
 class Direction(Enum):
+
     INCREASE = 1
     DECREASE = 2
 
 
 class Role(Enum):
+    """Enum, which contains all roles of users"""
     OWNER = "OWNER",
     ADMIN = "ADMIN",
     USER = "USER"
 
     @staticmethod
     def role_from_string(string_role: str):
+        """Convert string repr of role to Role object"""
         for value in Role.mro()[0]:
             if value.name == string_role.upper():
                 return value
@@ -22,3 +25,9 @@ class Role(Enum):
 
     def __str__(self):
         return self.name
+
+
+class BanType(Enum):
+    """Enum, which contains all ban types for users"""
+    TEMPORARY = "TEMPORARY",
+    PERMANENT = "PERMANENT"
