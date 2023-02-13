68cea6df9a78a03ae20b37e126ba048f63c468a8
Enj0yer <kulagin2002ilya@gmail.com>
2023-02-13 18:30:22 +0400

    - Add router

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import asyncio
 
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
+from bot.common import router as common_router
 
 from config.hidden import TOKEN
 
@@ -11,6 +12,7 @@ async def main():
     logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(name)s - %(message)s")
     bot = Bot(token=TOKEN)
     dp = Dispatcher(storage=MemoryStorage())
+    dp.include_router(common_router)
 
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
