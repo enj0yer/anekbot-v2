fc7558978e4202e0542c5f710c869362ba232072
Enj0yer <kulagin2002ilya@gmail.com>
2023-02-13 20:39:51 +0400

    - Remove useless code

--- config/__init__.py
+++
@@ -1,7 +0,0 @@
-from os import path
-
-if path.exists("hidden.py"):
-    pass
-else:
-    with open('hidden.py', 'w', encoding='utf-8') as file:
-        file.write('PATH = "%sqlite database path%"\nTOKEN = "%bot token%"')
