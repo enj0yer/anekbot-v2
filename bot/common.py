02bd0fcefd57e33f54fdb876843cb9eb8f7348db
Enj0yer <kulagin2002ilya@gmail.com>
2023-02-13 18:33:02 +0400

    ! Nothing works

--- bot/common.py
+++ bot/common.py
@@ -1,13 +1,33 @@
 from aiogram import Router, types
+from aiogram.filters import Command
 
-from classes.controllers import process_user
+from classes.controllers import process_user, JokesController
+from bot.service import Answers
 
-ROUTER = Router()
+router = Router()
 
 
+@router.message(Command(commands=['start']))
 async def welcome(message: types.Message):
+    result = process_user(message)
+    if not result:
+        return
+    await message.answer(Answers.welcome(message))
+
+
+@router.message(Command(commands=['joke']))
+async def get(message: types.Message):
+    result = process_user(message)
+    if not result:
+        return
+    controller = JokesController()
+    random_joke = controller.get_random_joke()
+    await message.answer(random_joke.data)
+
+
+@router.message()
+async def unknown(message: types.Message):
     result = process_user(message, save_changes=True)
     if not result:
         return
-    await message.answer(f"Приветствую, {(('@' + message.from_user.username) if message.from_user.username else 'Ghost')}"
-                         f"\n\n/joke для получения анекдота\n/help для просмотра остальных возможностей возможностей")
+    await message.answer(Answers.unknown())
